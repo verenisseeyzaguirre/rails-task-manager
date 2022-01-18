834bed3b726bba79c270770b8caa76fe36907eea
Claudia Lujan <verenisse.eyzaguirre@gmail.com>
2022-01-18 13:22:28 -0500

    new and create methods, add a new task

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -5,4 +5,19 @@ class TaskController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+  def new
+    @task = Task.new
+  end
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path()
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'tasks', to: 'task#index'
 
+  #Create
+  get 'tasks/new', to: 'task#new', as: :new_task
+  post 'tasks', to: 'task#create'
+
   # Ver detalles de un task
   get 'tasks/:id', to: 'task#show', as: :task
+
+
 end
