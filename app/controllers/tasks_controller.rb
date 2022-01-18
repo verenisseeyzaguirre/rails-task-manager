b36439422f36d13e991faaaf6dfca5c42dee7b37
Claudia Lujan <verenisse.eyzaguirre@gmail.com>
2022-01-18 15:23:56 -0500

    destroy singular name controller new one and refactoring

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,42 @@
+class TasksController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+  def index
+    @tasks = Task.all
+  end
+  def show
+  end
+  def new
+    @task = Task.new
+  end
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+
+    # si lo comento , aún se pinta luego de eliminado, otra forma q no sea redirect?
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set_restaurant
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'tasks', to: 'task#index'
+  # get 'tasks', to: 'task#index'
 
-  # Create
-  get 'tasks/new', to: 'task#new', as: :new_task
-  post 'tasks', to: 'task#create'
+  # # Create
+  # get 'tasks/new', to: 'task#new', as: :new_task
+  # post 'tasks', to: 'task#create'
 
-  # Ver detalles de un task
-  get 'tasks/:id', to: 'task#show', as: :task
+  # # Ver detalles de un task
+  # get 'tasks/:id', to: 'task#show', as: :task
 
-  # Update
-  get 'tasks/:id/edit', to: 'task#edit', as: :edit_task
-  patch 'tasks/:id', to: 'task#update'
+  # # Update
+  # get 'tasks/:id/edit', to: 'task#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'task#update'
+
+  # # Delete
+  # delete 'tasks/:id', to: 'task#destroy'
+
+  resources :tasks
 
 end
