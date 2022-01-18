f453137157c6c3ee511b3c11fd87877b442135ab
Claudia Lujan <verenisse.eyzaguirre@gmail.com>
2022-01-18 16:13:14 -0500

    type fixed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
@@ -32,7 +32,7 @@ class TasksController < ApplicationController
 
   private
 
-  def set_restaurant
+  def set_task
     @task = Task.find(params[:id])
   end
 
