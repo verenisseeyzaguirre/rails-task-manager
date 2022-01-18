a1a02105fc681de7b07d4578dec661e564f0af5b
Claudia Lujan <verenisse.eyzaguirre@gmail.com>
2022-01-18 14:06:58 -0500

    update partially implemented

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -11,7 +11,16 @@ class TaskController < ApplicationController
   def create
     @task = Task.new(task_params)
     @task.save
-    redirect_to tasks_path()
+    redirect_to tasks_path
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+
+    redirect_to task_path(@task)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'tasks', to: 'task#index'
 
-  #Create
+  # Create
   get 'tasks/new', to: 'task#new', as: :new_task
   post 'tasks', to: 'task#create'
 
   # Ver detalles de un task
   get 'tasks/:id', to: 'task#show', as: :task
 
+  # Update
+  get 'tasks/:id/edit', to: 'task#edit', as: :edit_task
+  patch 'tasks/:id', to: 'task#update'
 
 end
